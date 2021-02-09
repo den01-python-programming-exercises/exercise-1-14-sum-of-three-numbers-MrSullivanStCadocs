2710a58509c95371bb37cb1b4dde8ae412134ab2
MrSullivan <rsullivan@stcadocs.com>
2021-02-09 14:49:28 +0000

    three int iinputs and sum of.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
-    #write your code below this line
+    #write your code below this line:
+    first = int(input("What is your first number? "))
+    second = int(input("What is your second number? "))
+    third = int(input("What is your third number? "))
+    sum = first + second + third 
+    print("The sum of the numbers is: " + str(sum))
+    
 
 if __name__ == '__main__':
     main()
